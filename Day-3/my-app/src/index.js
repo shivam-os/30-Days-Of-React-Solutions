bcbec727cc8c5733cc9d3f9ea30a4c83ff0a383a
Shivam <shivam89raj45@gmail.com>
2022-09-11 22:57:44 +0530

    Add React app boilerplate

---
+++ Day-3/my-app/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const page = <h1>Hi i aM sHIVAM</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(page);
