6ee82bab9711f78370898ef62e5840e9886d1c9e
Shivam <shivam89raj45@gmail.com>
2022-09-13 22:03:08 +0530

    Finish level 1

---
+++ Day-4/level1.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function SuccessAlert() {
+  return (
+    <div>
+      <p>Success alert box!!!</p>
+    </div>
+  );
+}
+
+const WarningAlert = () => {
+  return (
+    <div>
+      <p>Warning alert box!!!</p>
+    </div>
+  )
+};
