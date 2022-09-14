26ac9aa429d4792d53f29ce108c77fc8ff7ab84e
Shivam <shivam89raj45@gmail.com>
2022-09-14 14:17:05 +0530

    Finish level 3

--- Day-4/level-3/src/index.js
+++ Day-4/level-3/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import asabenehPic from "./images/asabeneh.jpg";
 import "./index.css";
 
 /* Hexadecimal random color generator */
@@ -23,7 +24,7 @@ function HexColorBox() {
 
 function ColorsContainer() {
   return (
-    <div class="colors-container">
+    <div className="colors-container">
       <HexColorBox />
       <HexColorBox />
       <HexColorBox />
@@ -33,11 +34,50 @@ function ColorsContainer() {
   )
 }
 
-/* */
+/* User Card */
+function UserProfile() {
+  return (
+    <div>
+      <img className="user-profile" src={asabenehPic} />
+      <p className="user-name">ASABENEH YETAYEH</p>
+      <p className="subtitles">Senior Developer, Finland</p>
+    </div>
+  )
+}
+
+let skills = ["HTML", "CSS", "Sass", "JS", "React", "Redux", "Node", "MongoDB", "Python", "Flask", "Django", "NumPy", "Pandas", "Data Analysis", "MYSQL", "GraphQL", "Gatsby", "Docker", "Heroku", "Git"];
+
+const SkillsBox = skills.map((skill) => <div className="skills-box" key={skill}>{skill}</div>);
+
+function UserSkills() {
+  return (
+    <div>
+      <h2>Skills</h2>
+      {SkillsBox}
+      <p className="subtitles">Joined on Sept 14, 2022</p>
+    </div>
+  )
+}
+
+function UserCard() {
+  return (
+    <div className="user-card">
+      <UserProfile />
+      <UserSkills />
+    </div>
+  )
+}
+
+/* Rendering the componenets */
 
 function Page() {
   return (
-    <ColorsContainer />
+    <div>
+      <ColorsContainer />
+      <hr />
+      <UserCard />
+    </div>
+    
   );
 }
 
